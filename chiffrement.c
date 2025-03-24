f14017391919c6687432e1bf8d5bf9c1a62bb1e7
lucasrecan <lucas.recan@univ-tlse3.fr>
2025-03-24 15:43:54 +0100

    ajout de chiffrementCesar dans chiffrement.c

--- chiffrement.c
+++ chiffrement.c
@@ -2,17 +2,17 @@
 *  ASR => 4R2.04                                                              *
 *******************************************************************************
 *                                                                             *
-*  N° de Sujet :                                                              *
+*  N° de Sujet : 3                                                            *
 *                                                                             *
 *******************************************************************************
 *                                                                             *
-*  Intitulé :                                                                 *
+*  Intitulé : Chiffrement de messages                                         *
 *                                                                             *
 *******************************************************************************
 *                                                                             *
-*  Nom-prénom1 :                                                              *
+*  Nom-prénom1 : Chicano-Quéralta Matéo                                       *
 *                                                                             *
-*  Nom-prénom2 :                                                              *
+*  Nom-prénom2 : Récan Lucas                                                  *
 *                                                                             *
 *  Nom-prénom3 :                                                              *
 *                                                                             *
@@ -20,6 +20,19 @@
 *                                                                             *
 *******************************************************************************
 *                                                                             *
-*  Nom du fichier :                                                           *
+*  Nom du fichier : chiffrement.c                                             *
 *                                                                             *
 ******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAILLE_MAX 1000
+#define ALPHABET "abcdefghijklmnopqrstuvwxyz" 
+
+char* chiffrementCesar(char* message, int cle) {
+    char messageDechiffre[TAILLE_MAX];
+    for (int i=0; i<strlen(message); i++) {
+        
+        messageDechiffre[i] += ALPHABET[strchr(ALPHABET, message[i]) - ALPHABET + cle];
+    }
+}
