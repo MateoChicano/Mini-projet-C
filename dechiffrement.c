bd51c92e3ff4cf06b042d946fac9bd104305df21
Pinou <chicmat4@gmail.com>
2025-03-31 15:08:01 +0200

    Update dechiffrement.c

--- dechiffrement.c
+++ dechiffrement.c
@@ -31,9 +31,36 @@
 #define TAILLE_MAX 1000
 #define ALPHABET "abcdefghijklmnopqrstuvwxyz"
 
-void dechiffrementCesar(char message[], int cle){
+void main () {
     char message[TAILLE_MAX];
-    int i;
-    for (i =0, strlen(message))
+    printf("Entrez le message à déchiffrer : ");
+    fgets(message, TAILLE_MAX, stdin);//On entre un message
+    printf("%d\n", verification(message));
+    if (verification(message) == 0) {
+        perror("Erreur : Le message contient un caractère spécial\n");
+        exit(1);
+    } else {
+        dechiffrementCesar(message, cle, messageDechiffre);
+    } 
+    
 
+}
+void chiffrementCesar(char message[], int cle, char messageDechiffre[]) {
+    for (int i=0; i<strlen(message); i++) {
+        if (isIn(message[i], aGarder, 5) == 0) {
+            char nouveauCaractere = message[i] - cle;
+            messageDechiffre[i] = (message[i] - cle)%122;
+        } else {
+            // Les espaces sont écrits tel quel
+            messageDechiffre[i] = message[i];
+        }
+    }
+}
+
+void chiffrementVigenere(char message[], char cle[], char messageDechiffre[]) {
+    for (int i=0; i<strlen(message); i++) {
+        if (!messageDechiffre[i] == 32) {
+            messageDechiffre[i] = message[i] + cle[i%strlen(cle)];
+        }
+    }
 }
